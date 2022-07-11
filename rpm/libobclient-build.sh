69ad628185f3d064c3f454859cdf725c0a13932e
wangzelin.wzl <wangzelin.wzl@alibaba-inc.com>
2022-07-11 17:33:29 +0800

    update rpm

--- rpm/libobclient-build.sh
+++ rpm/libobclient-build.sh
@@ -3,23 +3,10 @@
 # Usage: obclient-build.sh <oceanbasepath> <package> <version> <release>
 # Usage: obclient-build.sh
 
-REDHAT=`cat /etc/redhat-release|cut -d " " -f 7|cut -d "." -f 1`
-
-if [ $# -ne 4 ]
-then
-  TOP_DIR=`pwd`/../
-  PACKAGE=$(basename $0 -build.sh)
-  VERSION=`cat ${PACKAGE}-VER.txt`
-  RELEASE="1"
-#RELEASE="dev.el${REDHAT}"
-else
-  TOP_DIR=$1
-  PACKAGE=$2
-  VERSION=$3
-  RELEASE="$4.el${REDHAT}"
-  export AONE_BUILD_NUMBER=${4}
-fi
-echo "[BUILD] args: TOP_DIR=${TOP_DIR} PACKAGE=${PACKAGE} VERSION=${VERSION} RELEASE=${RELEASE} AONE_BUILD_NUMBER=${AONE_BUILD_NUMBER}"
+TOP_DIR=$1
+PACKAGE=$2
+VERSION=$3
+RELEASE=$4
 
 TMP_DIR=${TOP_DIR}/${PACKAGE}-tmp.$$
 BOOST_DIR=${TMP_DIR}/BOOST
